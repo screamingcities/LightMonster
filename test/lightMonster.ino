e2e8d2492c37da611c71c5c0bf0f46c3558822aa
Jason S <screamingcities@gmail.com>
2015-01-22 17:41:06 -0500

    TEsting

---
+++ test/lightMonster.ino
@@ -0,0 +1,33 @@
+
+#include "rf_outlet_control.h"
+
+int outPin = D1;
+Remote remote(outPin);
+
+void setup() {
+
+    Spark.function("LightMonster", lightMonster);
+
+}
+
+void loop() {
+
+}
+
+//Send the signal to the lights.
+int lightMonster(String args){
+    int type;
+    char id;
+    boolean state = false;
+    type = args.charAt(0)-'0';
+    id = args.charAt(1);
+
+    if(args.substring(2).equals("ON")){
+        state = true;
+    }
+
+    remote.sendSignal(id, state);
+
+
+    return type;
+}
